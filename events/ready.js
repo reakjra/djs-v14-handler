2e48c2ab3e8fa3804932456ea713b47c135dc45c
Sayukee <85903730+Sayukee@users.noreply.github.com>
2022-08-06 01:29:59 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,20 +5,16 @@ const config = require('../config.json')
 Client.on('ready', async() => {
 
     console.log(`Logged in as ${Client.user.tag}!`)
+    
+  let data = Client.SlashCmds
 
-    let data = Client.SlashCmds
-
-    data.map(async(cmd) => {
-
-        if(cmd.Globally == true) {
-
-             return await Client.application?.commands.set(data);
-            
-        } else if(cmd.Globally == false) {
-
-           return await Client.guilds.cache.get(config.testGuildId).commands.set(data)            
+  let filt1 = data.filter(c => c.Globally == true)
            
-        }
-    })
+  Client.application?.commands.set(filt1)
+           
+  let filt2 = data.filter(c => c.Globally == false)
+            
+  await Client.guilds.cache.get(config.testGuildId).commands.set(filt2)            
+            
 })
 
